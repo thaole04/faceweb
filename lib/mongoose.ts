6e9e00b01f9fddd18ce473f9c748406bc440a6b6
thaole04 <thao.le14758@hcmut.edu.vn>
2023-11-05 14:57:35 +0700

    update mongoose function

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -15,7 +15,10 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {});
+    await mongoose.connect(
+      'mongodb+srv://thaole04:041003@facerecognition.tcpw49j.mongodb.net/mydatabase?retryWrites=true&w=majority',
+      {}
+    );
 
     isConnected = true; // Set the connection status to true
     console.log('MongoDB connected');
